07568d7dea01f464aa43f9dee328c5f139db90e1
Alex Hixon <ahixon@atlassian.com>
2018-09-07 16:15:13 +1000

    fix width for wrapped images greater than image size

--- packages/editor/editor-common/src/ui/MediaSingle/styled.ts
+++ packages/editor/editor-common/src/ui/MediaSingle/styled.ts
@@ -25,11 +25,12 @@ function calcWidth(
   layout: MediaSingleLayout,
   width: number,
   containerWidth?: number,
+  columnSpan?: number,
 ): string {
   switch (layout) {
     case 'wrap-right':
     case 'wrap-left':
-      return width > akEditorFullPageMaxWidth / 2
+      return width > akEditorFullPageMaxWidth / 2 && !columnSpan
         ? 'calc(50% - 12px)'
         : `${width}px`;
     case 'wide':
@@ -76,6 +77,7 @@ export interface WrapperProps {
   width: number;
   height: number;
   containerWidth?: number;
+  columnSpan?: number;
 }
 
 /**
@@ -87,9 +89,10 @@ const MediaSingleDimensionHelper = ({
   height,
   layout,
   containerWidth = 0,
+  columnSpan = 0,
 }: WrapperProps) => css`
   max-width: ${calcMaxWidth(layout, width, containerWidth)};
-  width: ${calcWidth(layout, width, containerWidth)};
+  width: ${calcWidth(layout, width, containerWidth, columnSpan)};
   float: ${float(layout)};
   margin: ${calcMargin(layout)};
 
