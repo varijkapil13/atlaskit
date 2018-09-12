67a689e093dec3d471e5c6f4f131099a3e734706
Ben Conolly <ben@thinkmill.com.au>
2018-09-12 17:10:49 +1000

    formally use build script to remove object and file type icons

--- packages/core/icon/build/index.js
+++ packages/core/icon/build/index.js
@@ -1,12 +1,15 @@
 // @flow
 const path = require('path');
 const buildIcons = require('@atlaskit/icon-build-process');
+const pkgDir = require('pkg-dir');
+
+const root = pkgDir.sync();
 
 const config = {
   // Relative to this directory
-  srcDir: path.resolve('../utils/raw_svgs'),
-  processedDir: path.resolve('../svgs'),
-  destDir: path.resolve('../glyph'),
+  srcDir: path.resolve(root, 'utils/raw_svgs'),
+  processedDir: path.resolve(root, 'svgs'),
+  destDir: path.resolve(root, 'glyph'),
 };
 
 buildIcons(config).then(icons => {
